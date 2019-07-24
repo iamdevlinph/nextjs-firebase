61299ed770e8fa06629d38be9cca2f6f515d7037
iamdevlinph <iamdevlinph@gmail.com>
2019-07-25 00:05:58 +0800

    getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { postsActions } from '../src/ducks/posts';
 
-const IndexPage = () => <div>Something Here</div>;
+const IndexPage = props => {
+  const { posts } = props;
+  return (
+    <div>
+      <h2>Index Page</h2>
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
+    </div>
+  );
+};
+
+IndexPage.getInitialProps = async ({ ctx }) => {
+  const { store } = ctx;
+  store.dispatch(postsActions.requestPosts());
+
+  await new Promise(resolve => {
+    const unsubscribe = store.subscribe(() => {
+      const { posts } = store.getState();
+      if (posts) {
+        unsubscribe();
+        resolve();
+      }
+    });
+
+    const { posts } = store.getState();
+    return {
+      posts,
+    };
+  });
+};
 
 export default IndexPage;
+
+IndexPage.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
